8139965bd6daa1579831ae970b1554bd4bd41556
alfonsopintos <alfonsopintos@gmail.com>
2018-02-27 13:12:07 -0500

    commit before bird physics

---
+++ Birds/Configuration.swift
@@ -0,0 +1,25 @@
+//
+//  Configuration.swift
+//  Birds
+//
+//  Created by Alfonso Pintos on 2/27/18.
+//  Copyright © 2018 Pepe Loves Books. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGPoint {
+    
+    static public func * (left: CGPoint, right: CGFloat) -> CGPoint {
+        return CGPoint(x: left.x * right, y: left.y * right)
+    }
+    
+    static public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+    
+    static public func - (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+ 
+}
--- Birds/GameScene.swift
+++ Birds/GameScene.swift
@@ -11,18 +11,40 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let gameCamera = SKCameraNode()
+    var mapNode = SKTileMapNode()
+    
+    let gameCamera = GameCamera()
     var panRecognizer = UIPanGestureRecognizer()
+    var pinchRecognizer = UIPinchGestureRecognizer()
+    var maxScale: CGFloat = 0
+    
+    var bird = Bird(type: .red)
+    let anchor = SKNode()
     
     override func didMove(to view: SKView) {
-        addCamera()
-        setupGestureRecognizer()
+        setupLevel()
+        setupGestureRecognizers()
     }
     
-    func setupGestureRecognizer() {
+    func setupGestureRecognizers() {
         guard let view = view else { return }
+        
         panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
         view.addGestureRecognizer(panRecognizer)
+        
+        pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        view.addGestureRecognizer(pinchRecognizer)
+    }
+    
+    func setupLevel() {
+        if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode {
+            self.mapNode = mapNode
+            maxScale = mapNode.mapSize.width / frame.size.width
+        }
+        addCamera()
+        anchor.position = CGPoint(x: mapNode.frame.midX / 2, y: mapNode.frame.midY / 2)
+        addChild(anchor)
+        addBird()
     }
     
     func addCamera() {
@@ -30,6 +52,12 @@ class GameScene: SKScene {
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width / 2, y: view.bounds.size.height / 2)
         camera = gameCamera
+        gameCamera.setConstraints(with: self, and: mapNode.frame, to: nil)
+    }
+    
+    func addBird() {
+        bird.position = anchor.position
+        addChild(bird)
     }
     
 }
@@ -38,6 +66,38 @@ class GameScene: SKScene {
 extension GameScene {
     
     @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view) * gameCamera.yScale
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x,
+                                      y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        guard let view = view else { return }
         
+        if sender.numberOfTouches == 2 {
+            let locationInView = sender.location(in: view)
+            let location = convertPoint(fromView: locationInView)
+            
+            if sender.state == .changed {
+                let convertedScale = 1/sender.scale
+                let newScale = gameCamera.yScale * convertedScale
+                if newScale < maxScale && newScale > 0.5 { gameCamera.setScale(newScale) }
+                
+                
+                let locationAfterScale = convertPoint(fromView: locationInView)
+                let locationDelta = location - locationAfterScale
+                let newPosition = gameCamera.position + locationDelta
+                gameCamera.position = newPosition
+                sender.scale = 1.0
+                gameCamera.setConstraints(with: self, and: mapNode.frame, to: nil)
+            }
+        }
     }
 }
+
+
+
+
+
